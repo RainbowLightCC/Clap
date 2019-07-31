a0e4d7daf9cc7bcb9b7e2f075ee06dee57c2d024
TheFunnyCrafter <2827093931@qq.com>
2019-07-31 22:34:29 +0800

    Change from Eclipse to Idea; Added Maven.

---
+++ Clap/src/main/java/cn/funnymc/Main.java
@@ -0,0 +1,20 @@
+package cn.funnymc;
+
+import java.net.UnknownHostException;
+
+import cn.funnymc.server.*;
+import cn.funnymc.login.*;
+/**
+ * SocketChat 程序入口
+ * @author lcj
+ */
+public class Main {
+	public static void main(String[] args) throws UnknownHostException {
+		System.out.println("Starting Clapping Game Server...");
+		//在10087端口上启动Clap服务器
+		new Clap(10087).start();
+		//初始化MySQL
+		MySQL.getPassword();
+		MySQL.getConn();
+	}
+}
