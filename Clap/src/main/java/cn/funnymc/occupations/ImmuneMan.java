82aba8c7491ef045b19ae1c90071bc2aac18e642
TheFunnyCrafter <2827093931@qq.com>
2019-08-15 12:40:11 +0800

    免疫系统

---
+++ Clap/src/main/java/cn/funnymc/occupations/ImmuneMan.java
@@ -0,0 +1,30 @@
+package cn.funnymc.occupations;
+
+import cn.funnymc.actions.Attack;
+
+import java.util.Set;
+
+/**
+ * Abstract class of occupations that supports immune system.
+ */
+public abstract class ImmuneMan extends UnemployedMan{
+    private Set<String> immuneSet;
+    public ImmuneMan(int health, String name){
+        super(health, name);
+    }
+    
+    @Override
+    protected boolean doHarm(Attack attack){
+        if(immuneSet.contains(attack.attribute))return false;
+        this.health-=attack.harm;
+        return true;
+    }
+        
+    protected void setImmuneSet(Set<String> immuneSet) {
+        this.immuneSet = immuneSet;
+    }
+
+    public Set<String> getImmuneSet() {
+        return immuneSet;
+    }
+}
